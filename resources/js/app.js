58e94141f1cb398fadb7bbd631ef829de2b688c7
Olajide Eniseyin <eniseyinolajide@gmail.com>
2024-09-26 05:26:16 +0100

    added vue3-select, vue-toastification & fontawesome packages

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,16 @@
 import './bootstrap';
 import '../css/app.css';
 
+import '@fortawesome/fontawesome-free/css/all.css';
+
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+
+import Toast from 'vue-toastification'; // Import vue-toastification
+import 'vue-toastification/dist/index.css'; // Import the CSS for Toastification
+
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
@@ -15,6 +21,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .use(Toast, options) // Register Vue Toastification
             .mount(el);
     },
     progress: {
