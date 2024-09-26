5e70db5ac19bdd702e74c5263fb0106d77f0c0f2
Olajide Eniseyin <eniseyinolajide@gmail.com>
2024-09-26 04:13:44 +0100

    added OrderItem model & migration, updated hmo and orders migration

--- database/migrations/2024_09_26_030944_create_order_items_table.php
+++ database/migrations/2024_09_26_030944_create_order_items_table.php
@@ -11,11 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('orders', function (Blueprint $table) {
+        Schema::create('order_items', function (Blueprint $table) {
             $table->id();
-            $table->string('hmo_code');
-            $table->foreignId('provider');
-            $table->date('encounter_date');
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('orders');
+        Schema::dropIfExists('order_items');
     }
 };
