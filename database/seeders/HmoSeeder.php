1ffd7e5cec87a263ef1403779fa44f00c8374cc8
Olajide Eniseyin <eniseyinolajide@gmail.com>
2024-10-10 16:04:46 +0100

    updated hmo seeder so it can populate batching criteria

--- database/seeders/HmoSeeder.php
+++ database/seeders/HmoSeeder.php
@@ -6,14 +6,15 @@ use Faker\Factory as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use App\Enums\BatchingCriteria;
 
 class HmoSeeder extends Seeder
 {
     private $hmos = [
-        ['name'=>'HMO A', 'code'=> 'HMO-A'],
-        ['name'=>'HMO B', 'code'=> 'HMO-B'],
-        ['name'=>'HMO C', 'code'=> 'HMO-C'],
-        ['name'=>'HMO D', 'code'=> 'HMO-D'],
+        ['name' => 'HMO A', 'code' => 'HMO-A', 'batching_criteria' => BatchingCriteria::DAY_OF_SUBMISSION],
+        ['name' => 'HMO B', 'code' => 'HMO-B'],
+        ['name' => 'HMO C', 'code' => 'HMO-C'],
+        ['name' => 'HMO D', 'code' => 'HMO-D'],
     ];
 
     /**
@@ -23,12 +24,11 @@ class HmoSeeder extends Seeder
     {
         $faker = Faker::create();
 
-        $hmosWithEmails = array_map(function($hmo) use ($faker) {
-            return [
-                'name' => $hmo['name'],
-                'code' => $hmo['code'],
+        $hmosWithEmails = array_map(function ($hmo) use ($faker) {
+            return array_merge($hmo, [
                 'email' => $faker->unique()->safeEmail, // Generate a random unique email
-            ];
+                'batching_criteria' => $hmo['batching_criteria'] ?? BatchingCriteria::MONTH_OF_ENCOUNTER,
+            ]);
         }, $this->hmos);
 
         DB::table('hmos')->insert($hmosWithEmails);
