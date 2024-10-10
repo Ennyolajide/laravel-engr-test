9dea5a1497191c65cb5f20383173d8f3a6d9c947
Olajide Eniseyin <eniseyinolajide@gmail.com>
2024-10-10 16:01:31 +0100

    created batches migration

---
+++ database/migrations/2024_10_09_155358_create_batches_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('batches', function (Blueprint $table) {
+            $table->id();
+            $table->string('batch_key'); // The unique identifier for the batch (e.g., "Provider A Jan 2021")
+            $table->foreignId('hmo_id')->constrained()->onDelete('cascade');// Foreign key referencing hmos
+            $table->enum('status', ['pending', 'processed'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('batches');
+    }
+};
