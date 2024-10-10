de21130b1ab586db5b12d51fbc8af236b6c42f48
Olajide Eniseyin <eniseyinolajide@gmail.com>
2024-10-10 16:00:58 +0100

    added batching_criteria column to hmo migration

--- database/migrations/2024_07_18_135307_create_hmos_table.php
+++ database/migrations/2024_07_18_135307_create_hmos_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('code')->unique();
             $table->string('email')->unique();
+            $table->string('batching_criteria')->default('month_of_encounter');
             $table->timestamps();
         });
     }
